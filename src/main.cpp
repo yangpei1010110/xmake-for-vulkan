978f497fc812411f7a9ca82ecaac1d86e22bdee9
yangpei1010110 <970387216@qq.com>
2024-01-18 17:39:17 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    DWORD test = 0;
+    std::cout << "init on xmake!" << test << std::endl;
+    return 0;
+}
